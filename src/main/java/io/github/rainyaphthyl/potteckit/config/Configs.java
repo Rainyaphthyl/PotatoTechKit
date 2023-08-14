64f0900eeb618a134e9d75a367d8c0feaf42ed30
Natfalluvia <amena_rny@outlook.com>
2023-08-15 02:39:12 +0800

    Add config `chunkLoadingGraph`

--- src/main/java/io/github/rainyaphthyl/potteckit/config/Configs.java
+++ src/main/java/io/github/rainyaphthyl/potteckit/config/Configs.java
@@ -16,6 +16,8 @@ public class Configs {
     public static final HotkeyedBooleanConfig enablePotteckit = new HotkeyedBooleanConfig("enablePotteckit", true, "");
     @Config(types = Type.HOTKEY, domains = Domain.GENERIC, notVanilla = false, cheating = false)
     public static final HotkeyConfig openConfigScreen = new HotkeyConfig("openConfigScreen", "K,C");
+    @Config(types = {Type.TOGGLE, Type.HOTKEY}, domains = Domain.METER, needServer = true, notVanilla = false, cheating = false)
+    public static final HotkeyedBooleanConfig chunkLoadingGraph = new HotkeyedBooleanConfig("chunkLoadingGraph", false, "");
 
     public static void registerOnInit() {
         JsonModConfig jsonModConfig = new JsonModConfig(Reference.MOD_INFO, Reference.CONFIG_VERSION, ConfigHandler.optionCategoryList);
