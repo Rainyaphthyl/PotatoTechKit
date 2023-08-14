7da887b693d4859f9075083f20ce39b918bde884
Natfalluvia <amena_rny@outlook.com>
2023-08-15 01:32:36 +0800

    Change category order

--- src/main/java/io/github/rainyaphthyl/potteckit/config/annotation/Category.java
+++ src/main/java/io/github/rainyaphthyl/potteckit/config/annotation/Category.java
@@ -9,22 +9,22 @@ public enum Category {
      * Helping players to see more info
      */
     METER("Meters", "meter"),
+    FIX("Fixes", "fix"),
+    TWEAK("Tweaks", "tweak"),
+    YEET("Yeet", "yeet"),
     /**
      * Helping players with game operations
      */
     ACTION("Actions", "action"),
-    FIX("Fixes", "fix"),
-    TWEAK("Tweaks", "tweak"),
-    YEET("Yeet", "yeet"),
     WITH_SERVER("NeedServer", "server"),
-    /**
-     * Destructive to some vanilla features, causing players to forget the vanilla operations and refuse to redstone with them; Or solving some vanilla problems that could have been solved with vanilla technologies.
-     */
-    NOT_VANILLA("NotVanilla", "vanilless"),
     /**
      * Making some manual operations so convenient that players refuse to design automatic machines for them.
      */
-    CHEATING("Cheating", "cheat");
+    CHEATING("Cheating", "cheat"),
+    /**
+     * Destructive to some vanilla features, causing players to forget the vanilla operations and refuse to redstone with them; Or solving some vanilla problems that could have been solved with vanilla technologies.
+     */
+    NOT_VANILLA("NotVanilla", "vanilless");
     public final String name;
     /**
      * {@code "potteckit.config.tab." + key (lowercase)}
