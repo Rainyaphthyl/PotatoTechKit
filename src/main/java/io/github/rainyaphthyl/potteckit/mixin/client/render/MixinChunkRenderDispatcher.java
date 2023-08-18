25dc0ae58829398ecaafc4296baf1df9eb2421f3
Natfalluvia <amena_rny@outlook.com>
2023-08-18 22:45:41 +0800

    Add profiler section `makeCompileTask`

--- src/main/java/io/github/rainyaphthyl/potteckit/mixin/client/render/MixinChunkRenderDispatcher.java
+++ src/main/java/io/github/rainyaphthyl/potteckit/mixin/client/render/MixinChunkRenderDispatcher.java
@@ -1,8 +1,32 @@
 package io.github.rainyaphthyl.potteckit.mixin.client.render;
 
+import io.github.rainyaphthyl.potteckit.config.Configs;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.chunk.ChunkCompileTaskGenerator;
 import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
+import net.minecraft.client.renderer.chunk.RenderChunk;
+import net.minecraft.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import javax.annotation.Nonnull;
 
 @Mixin(ChunkRenderDispatcher.class)
 public class MixinChunkRenderDispatcher {
+    @Unique
+    public final Profiler potatoTechKit$profiler = Minecraft.getMinecraft().profiler;
+
+    @Redirect(method = "updateChunkNow", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/chunk/RenderChunk;makeCompileTaskChunk()Lnet/minecraft/client/renderer/chunk/ChunkCompileTaskGenerator;"))
+    public ChunkCompileTaskGenerator profileCompile(@Nonnull RenderChunk chunkRenderer) {
+        if (Configs.enablePotteckit.getBooleanValue() && Configs.moreProfilerLevels.getBooleanValue()) {
+            potatoTechKit$profiler.startSection("makeCompileTask");
+            ChunkCompileTaskGenerator generator = chunkRenderer.makeCompileTaskChunk();
+            potatoTechKit$profiler.endSection();
+            return generator;
+        } else {
+            return chunkRenderer.makeCompileTaskChunk();
+        }
+    }
 }
