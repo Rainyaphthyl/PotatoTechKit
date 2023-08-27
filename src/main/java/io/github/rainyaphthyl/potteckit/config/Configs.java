0f2245f30974b7aec2a3ce9247d5c506ddca0778
Natfalluvia <amena_rny@outlook.com>
2023-08-27 12:17:54 +0800

    Better translation keys and client CLG switch

--- src/main/java/io/github/rainyaphthyl/potteckit/config/Configs.java
+++ src/main/java/io/github/rainyaphthyl/potteckit/config/Configs.java
@@ -13,13 +13,15 @@ import io.github.rainyaphthyl.potteckit.util.Reference;
 
 public class Configs {
     @Config(types = {Type.TOGGLE, Type.HOTKEY}, domains = Domain.GENERIC, notVanilla = false, cheating = false)
-    public static final HotkeyedBooleanConfig enablePotteckit = new HotkeyedBooleanConfig("enablePotteckit", true, "");
+    public static final HotkeyedBooleanConfig enablePotteckit = new HotkeyedBooleanConfig("enable_potteckit", true, "", "enable_potteckit", "enable_potteckit");
     @Config(types = Type.HOTKEY, domains = Domain.GENERIC, notVanilla = false, cheating = false)
-    public static final HotkeyConfig openConfigScreen = new HotkeyConfig("openConfigScreen", "K,C");
+    public static final HotkeyConfig openConfigScreen = new HotkeyConfig("open_config_screen", "K,C");
     @Config(types = {Type.TOGGLE, Type.HOTKEY}, domains = Domain.METER, needServer = true, notVanilla = false, cheating = false)
-    public static final HotkeyedBooleanConfig chunkLoadingGraph = new HotkeyedBooleanConfig("chunkLoadingGraph", false, "");
+    public static final HotkeyedBooleanConfig chunkLoadingGraph = new HotkeyedBooleanConfig("chunk_loading_graph", false, "", "chunk_loading_graph", "chunk_loading_graph");
     @Config(types = {Type.TOGGLE, Type.HOTKEY}, domains = Domain.METER, needServer = true, notVanilla = false, cheating = false)
-    public static final HotkeyedBooleanConfig chunkLoadingDetails = new HotkeyedBooleanConfig("chunkLoadingDetails", false, "");
+    public static final HotkeyedBooleanConfig chunkLoadingDetails = new HotkeyedBooleanConfig("chunk_loading_details", false, "", "chunk_loading_details", "chunk_loading_details");
+    @Config(types = {Type.TOGGLE, Type.HOTKEY}, domains = Domain.METER, notVanilla = false, cheating = false)
+    public static final HotkeyedBooleanConfig chunkLoadingGraphReceiver = new HotkeyedBooleanConfig("chunk_loading_graph_receiver", false, "", "chunk_loading_graph_receiver", "chunk_loading_graph_receiver");
 
     public static void registerOnInit() {
         JsonModConfig jsonModConfig = new JsonModConfig(Reference.MOD_INFO, Reference.CONFIG_VERSION, ConfigHandler.optionCategoryList);
