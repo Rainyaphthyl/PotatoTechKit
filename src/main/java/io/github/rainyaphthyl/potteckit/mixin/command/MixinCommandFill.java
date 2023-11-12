6b3cb22f046f728390354a0930f137e1d706f1a4
Natfalluvia <amena_rny@outlook.com>
2023-11-13 01:51:42 +0800

    Yeet block updates of command `fill`

---
+++ src/main/java/io/github/rainyaphthyl/potteckit/mixin/command/MixinCommandFill.java
@@ -0,0 +1,21 @@
+package io.github.rainyaphthyl.potteckit.mixin.command;
+
+import io.github.rainyaphthyl.potteckit.config.Configs;
+import net.minecraft.block.Block;
+import net.minecraft.command.CommandFill;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(CommandFill.class)
+public abstract class MixinCommandFill {
+    @Redirect(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;notifyNeighborsRespectDebug(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/Block;Z)V"))
+    public void forgetBlockUpdates(World instance, BlockPos pos, Block blockType, boolean updateObservers) {
+        boolean flag = Configs.yeetFillUpdate.getBooleanValue() && Configs.enablePotteckit.getBooleanValue();
+        if (!flag) {
+            instance.notifyNeighborsRespectDebug(pos, blockType, updateObservers);
+        }
+    }
+}
