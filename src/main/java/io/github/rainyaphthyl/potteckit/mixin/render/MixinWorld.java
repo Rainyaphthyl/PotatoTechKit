7cb616f39da0f51868ba5fd0ba43e23bc477858b
Natfalluvia <amena_rny@outlook.com>
2024-09-27 15:01:54 +0800

    Disable Travel Light Check only if playerSP==viewEntity

--- src/main/java/io/github/rainyaphthyl/potteckit/mixin/render/MixinWorld.java
+++ src/main/java/io/github/rainyaphthyl/potteckit/mixin/render/MixinWorld.java
@@ -9,6 +9,7 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,24 +20,33 @@ public abstract class MixinWorld {
     @Final
     public boolean isRemote;
 
+    @Unique
+    private static boolean potatoTechKit$needClientUpdate(Entity entity) {
+        boolean flag = false;
+        if (entity != null && entity.dimension == -1) {
+            if (entity.motionX * entity.motionX + entity.motionZ * entity.motionZ + entity.motionY * entity.motionY >= 0.3 * 0.3) {
+                flag = true;
+            } else {
+                Entity ridingEntity = entity.getRidingEntity();
+                if (ridingEntity != null) {
+                    if (ridingEntity.motionX * ridingEntity.motionX + ridingEntity.motionZ * ridingEntity.motionZ + ridingEntity.motionY * ridingEntity.motionY >= 0.3 * 0.3) {
+                        flag = true;
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
     @Inject(method = "checkLightFor", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/profiler/Profiler;startSection(Ljava/lang/String;)V", args = "ldc=checkedPosition < toCheckCount"), cancellable = true)
     public void yeetPositionsToCheck(EnumSkyBlock lightType, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         if (Configs.enablePotteckit.getBooleanValue() && this.isRemote && Configs.yeetTravelLightCheck.getBooleanValue()) {
-            Entity viewEntity = Minecraft.getMinecraft().getRenderViewEntity();
-            if (viewEntity != null && viewEntity.dimension == -1) {
-                if (viewEntity.motionX * viewEntity.motionX + viewEntity.motionZ * viewEntity.motionZ + viewEntity.motionY * viewEntity.motionY >= 0.3 * 0.3) {
-                    if (cir.isCancellable() && !cir.isCancelled()) {
-                        cir.setReturnValue(true);
-                    }
-                } else {
-                    Entity ridingEntity = viewEntity.getRidingEntity();
-                    if (ridingEntity != null) {
-                        if (ridingEntity.motionX * ridingEntity.motionX + ridingEntity.motionZ * ridingEntity.motionZ + ridingEntity.motionY * ridingEntity.motionY >= 0.3 * 0.3) {
-                            if (cir.isCancellable() && !cir.isCancelled()) {
-                                cir.setReturnValue(true);
-                            }
-                        }
-                    }
+            Minecraft client = Minecraft.getMinecraft();
+            Entity viewEntity = client.getRenderViewEntity();
+            if (client.player == viewEntity) {
+                boolean flag = potatoTechKit$needClientUpdate(viewEntity);
+                if (flag && cir.isCancellable() && !cir.isCancelled()) {
+                    cir.setReturnValue(true);
                 }
             }
         }
