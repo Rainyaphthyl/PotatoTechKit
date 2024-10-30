edf428c37eb542a77311ab4ac638fb2434e2d1e2
Natfalluvia <amena_rny@outlook.com>
2024-10-30 23:18:25 +0800

    New config tab: swap_elytra_chestplate

--- src/main/java/io/github/rainyaphthyl/potteckit/config/Configs.java
+++ src/main/java/io/github/rainyaphthyl/potteckit/config/Configs.java
@@ -128,6 +128,8 @@ public class Configs {
     public static final BooleanAndIntConfig dynamicFPS = new BooleanAndIntConfig("dynamic_fps", false, 5, 1, 20, "dynamic_fps");
     @Config(types = Type.TOGGLE, domains = Domain.YEET)
     public static final HotkeyedBooleanConfig yeetTravelLightCheck = new HotkeyedBooleanConfig("yeet_travel_light_check", false, "", "yeet_travel_light_check", "yeet_travel_light_check");
+    @Config(types = Type.TOGGLE, domains = Domain.TWEAK)
+    public static final HotkeyedBooleanConfig swapElytraChestplate = new HotkeyedBooleanConfig("swap_elytra_chestplate", false, "", "swap_elytra_chestplate", "swap_elytra_chestplate");
 
     public static void registerOnInit() {
         JsonModConfig jsonModConfig = new JsonModConfig(Reference.MOD_INFO, Reference.CONFIG_VERSION, ConfigHandler.optionCategoryList);
