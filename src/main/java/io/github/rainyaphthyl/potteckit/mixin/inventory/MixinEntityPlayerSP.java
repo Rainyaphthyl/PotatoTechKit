c01add0cbf4eba2fbafd9c9f6069f7d3ef37d56b
Natfalluvia <amena_rny@outlook.com>
2024-11-01 03:35:36 +0800

    Elytra Auto Deploy (reverse)

--- src/main/java/io/github/rainyaphthyl/potteckit/mixin/inventory/MixinEntityPlayerSP.java
+++ src/main/java/io/github/rainyaphthyl/potteckit/mixin/inventory/MixinEntityPlayerSP.java
@@ -7,14 +7,17 @@ import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
-import net.minecraft.item.ItemElytra;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,6 +25,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityPlayerSP.class)
 public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
+    @Shadow
+    private boolean wasFallFlying;
     @Unique
     private ItemStack potatoTechKit$cachedChestStack = ItemStack.EMPTY;
 
@@ -39,11 +44,10 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
                 int prevDurability = 0;
                 boolean prevMending = false;
                 for (int i = -1; i < 36; ++i) {
-                    int id = i == -1 ? 45 :
-                            (i < 9 ? i + 36 : i);
+                    int id = i == -1 ? 45 : (i < 9 ? i + 36 : i);
                     Slot currSlot = inventoryContainer.getSlot(id);
                     ItemStack currStack = currSlot.getStack();
-                    if (!currStack.isEmpty() && currStack.getItem() instanceof ItemElytra) {
+                    if (!currStack.isEmpty() && currStack.getItem() == Items.ELYTRA) {
                         int level = EnchantmentHelper.getEnchantmentLevel(Enchantments.UNBREAKING, currStack);
                         int damage = currStack.getItemDamage();
                         boolean mending = EnchantmentHelper.getEnchantmentLevel(Enchantments.MENDING, currStack) > 0;
@@ -79,6 +83,49 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
                         InventoryUtils.clickSlot(inventoryContainer, elytraSlot, currentItem, ClickType.SWAP);
                     }
                     potatoTechKit$cachedChestStack = chestStack;
+                }
+            }
+        }
+    }
+
+    @Inject(method = "notifyDataManagerChange", at = @At(value = "RETURN"))
+    public void autoDeployChestplate(DataParameter<?> key, CallbackInfo ci) {
+        if (Configs.enablePotteckit.getBooleanValue() && Configs.swapElytraChestplate.getBooleanValue()) {
+            if (FLAGS.equals(key) && !isElytraFlying() && wasFallFlying) {
+                // the new data have been refreshed during EntityDataManager.setEntryValue()
+                ItemStack elytraStack = getItemStackFromSlot(EntityEquipmentSlot.CHEST);
+                Slot chestSlot = null;
+                ItemStack chestStack = ItemStack.EMPTY;
+                if (elytraStack.getItem() == Items.ELYTRA) {
+                    for (int i = -1; i < 36; ++i) {
+                        int id = i == -1 ? 45 : (i < 9 ? i + 36 : i);
+                        Slot currSlot = inventoryContainer.getSlot(id);
+                        ItemStack currStack = currSlot.getStack();
+                        if (!currStack.isEmpty()) {
+                            Item currItem = currStack.getItem();
+                            if (currItem instanceof ItemArmor
+                                    && ((ItemArmor) currItem).armorType == EntityEquipmentSlot.CHEST
+                                    && InventoryUtils.areStacksEqualIgnoreDurability(potatoTechKit$cachedChestStack, currStack, false)) {
+                                chestSlot = currSlot;
+                                chestStack = currStack;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (chestSlot != null && !chestStack.isEmpty()) {
+                    Slot elytraSlot = inventoryContainer.getSlot(6);
+                    int slotNumber = chestSlot.slotNumber;
+                    if (slotNumber >= 36 && slotNumber < 45) {
+                        // hot-bar
+                        InventoryUtils.clickSlot(inventoryContainer, elytraSlot, slotNumber - 36, ClickType.SWAP);
+                    } else {
+                        int currentItem = inventory.currentItem;
+                        InventoryUtils.clickSlot(inventoryContainer, chestSlot, currentItem, ClickType.SWAP);
+                        InventoryUtils.clickSlot(inventoryContainer, elytraSlot, currentItem, ClickType.SWAP);
+                        InventoryUtils.clickSlot(inventoryContainer, chestSlot, currentItem, ClickType.SWAP);
+                    }
+                    potatoTechKit$cachedChestStack = ItemStack.EMPTY;
                 }
             }
         }
